90ea77ad5597cae30baebd30337179a0e3466b7e
BrewBarred <elayjahh7@hotmail.com>
2025-09-15 02:03:53 +1200

    Added basic locations to Varrock enum

--- scripts/locations/cities/Varrock.java
+++ scripts/locations/cities/Varrock.java
@@ -2,16 +2,33 @@ package locations.cities;
 
 import locations.TravelMan;
 import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.map.constants.Banks;
 
 public enum Varrock implements TravelMan {
-    SQUARE("Varrock square", new Area(3210, 3420, 3225, 3410), "The starting, and sometimes ending location for players. This is where the \"Home Teleport\" teleports you to in the normal spell-book."),
-    CASTLE("Varrock Castle", new Area(3200, 3460, 3215, 3450), "The heart of Lumbridge, and to some, the most nostalgic building in Gielnor. The castle has a spinning wheel for low-level Crafting tasks, a bank on the top-floor and a vast array of Trees behind it for easy starter levels.");
+    ARIS(new Area(3201, 3425, 3204, 3423), "Aris Maye (Fortuneteller)"),
+    CHARLIE_THE_TRAMP(new Area(3207, 3393, 3210, 3390), "Charlie the Tramp"),
+    EAST_BANK(Banks.VARROCK_EAST, "Varrock east-bank"),
+    GRAND_EXCHANGE(Banks.GRAND_EXCHANGE, "The Grand-Exchange", "The economy hub of old-school runescape! This is where non-restricted players (non-ironmen) can buy/sell items through a shared market. This place is also required to complete some clue-steps."),
+    SHOP_ARROWS(new Area(3230, 3425, 3233, 3421), "Lowe's Archery Emporium"),
+    SHOP_PLATES(new Area(3228, 3439, 3231, 3434), "Horvik's Armour Shop"),
+    SHOP_RUNES(new Area(3251, 3401, 3254, 3400), "Aubury's Rune Shop"),
+    SHOP_SWORDS(new Area(3205, 3400, 3207, 3397), "Varrock Swordshop"),
+    TOWN_SQUARE(new Area(3207, 3435, 3220, 3422), "Varrock town-square"),
+    WEST_ANVIL(new Area(3185, 3427, 3189, 3421), "Varrock anvil"),
+    WEST_BANK(Banks.VARROCK_WEST, "Varrock west-bank");
 
-    public final String name;
+
     public final Area area;
+    public final String name;
     public final String description;
 
-    Varrock(String name, Area area, String description) {
+    Varrock(Area area, String name) {
+        this.area = area;
+        this.name = name;
+        this.description = null;
+    }
+
+    Varrock(Area area, String name, String description) {
         this.area = area;
         this.name = name;
         this.description = description;
